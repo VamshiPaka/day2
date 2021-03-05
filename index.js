8dfcd255cb89132de6bf5b9346ae1dd0f644860d
VamshiPaka <vamshi.paka@gmail.com>
2021-03-05 10:25:16 +0530

    another function for added for creating array of arrays

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+/**function to create array of arrays */
 let createArrayOfArrays = (countryList) => {
   countryList.map((data, index) => {
     console.log(data.toLowerCase(), data.substring(0, 3), data.length);
   });
-
-  
 };
 const countries = [
   "ALBANIA",
@@ -19,3 +18,14 @@ const countries = [
   "KENYA",
 ];
 createArrayOfArrays(countries);
+
+countries.forEach(myFunction);
+function myFunction(item, index) {
+  var arr = new Array();
+  arr.push(item);
+  arr.push(item.slice(0, 3));
+  arr.push(item.length);
+  console.log(arr);
+}
+
+/**Using filter function to identify score above 85 */
