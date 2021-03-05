c7427f40b1543cb56ec32d8b22909391c8587c1b
VamshiPaka <vamshi.paka@gmail.com>
2021-03-05 10:37:56 +0530

    apply filter function on the user array to get score above 80

--- index.js
+++ index.js
@@ -29,3 +29,56 @@ function myFunction(item, index) {
 }
 
 /**Using filter function to identify score above 85 */
+
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTM"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+
+users.map((data, index) => {
+  if (data.scores > 85) {
+    console.log(data);
+  }
+});
+
+//console.log(users.filter((newuser) => newuser.scores > 85));
